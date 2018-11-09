983bae289fb02753f3dc9d3bf57f404a6cb199e1
Otarichi <gvazava911@gmail.com>
2018-11-09 16:10:21 +0400

    secondcommit

---
+++ mycalculator/app/src/main/java/gbs/mycalculator/MainActivity.kt
@@ -0,0 +1,19 @@
+package gbs.mycalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    } fun buttonClick (view: View) {
+        val button: Button = view as Button
+        resultTextView.text = resultTextView.text.toString() + button.text.toString()
+    }
+}
